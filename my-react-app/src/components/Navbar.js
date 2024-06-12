63f0ae8e8f0963f6709e429bb198c078bc778856
Faith Zhang <fz2*@rice.edu>
2024-06-12 15:17:09 -0500

    made the navbar

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Navbar.css';
 
 
 function Navbar(){
     return(
+        <div className = "navigation">
+            <div className= "initials"> FLZ</div>
 
-        <div>Faith Zhang</div>
+            <a href= '/about'> Home </a>
+            <a href= '/portfolio'> Portfolio </a>
+            <a href= '/Resume'> About </a>
+
+        </div>
+        
+        
     );
 
 }
